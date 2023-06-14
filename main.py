681f7c2c5bd0f26e346e5d252315748f94853617
Matheus Carneiro <m.carneiro.tech@gmail.com>
2023-06-14 20:54:15 -0300

    tts

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+
+import PyPDF2
+from art import *
+from cli_args_system import Args
+from gtts import gTTS
+
+print(text2art('PDF to MP3'))
+
+args = Args(convert_numbers=False)
+text_cleaned = ''
+language = 'pt-br'
+
+file_location = args.flag_str('file', 'f', 'File location')
+
+pdf_reader = PyPDF2.PdfReader(open(file_location, 'rb'))
+
+for page_num in range(len(pdf_reader.pages)):
+    text = pdf_reader.pages[page_num].extract_text()
+    text_cleaned = text.replace('\n', '')
+
+file_name = file_location.split('/')[-1].split('.')[0]
+
+time.sleep(15)
+
+recorder = gTTS(text=text_cleaned, lang=language).save(f'{file_name}.mp3')
