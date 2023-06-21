5b60939bcfd9629b302b36edfa0f2353c81cb522
Restyled.io <commits@restyled.io>
2023-06-21 01:40:04 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -5,27 +5,26 @@ from art import *
 from cli_args_system import Args
 from gtts import gTTS
 
-print(text2art('PDF to MP3'))
+print(text2art("PDF to MP3"))
 
 args = Args(convert_numbers=False)
-text_cleaned = ''
+text_cleaned = ""
 text_list = []
-language = 'pt-br'
+language = "pt-br"
 
-file_location = args.flag_str('file', 'f', 'File location')
+file_location = args.flag_str("file", "f", "File location")
 
-pdf_reader = PyPDF2.PdfReader(open(file_location, 'rb'))
+pdf_reader = PyPDF2.PdfReader(open(file_location, "rb"))
 
 for page_num in range(len(pdf_reader.pages)):
     text = pdf_reader.pages[page_num].extract_text()
-    text_cleaned = text.replace('\n', '')
+    text_cleaned = text.replace("\n", "")
     text_list.append(text_cleaned)
 
-file_name = file_location.split('/')[-1].split('.')[0]
+file_name = file_location.split("/")[-1].split(".")[0]
 
-print('Recording ...')
+print("Recording ...")
 time.sleep(15)
 
-recorder = gTTS(text=",".join(text_list),
-                lang=language).save(f'{file_name}.mp3')
-print('Done!')
+recorder = gTTS(text=",".join(text_list), lang=language).save(f"{file_name}.mp3")
+print("Done!")
