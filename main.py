ec53b1dca1d977c66f45aa10e197e63205c25a94
Matheus Carneiro <m.carneiro.tech@gmail.com>
2023-06-20 22:39:24 -0300

    now she reads everything

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print(text2art('PDF to MP3'))
 
 args = Args(convert_numbers=False)
 text_cleaned = ''
+text_list = []
 language = 'pt-br'
 
 file_location = args.flag_str('file', 'f', 'File location')
@@ -18,9 +19,12 @@ pdf_reader = PyPDF2.PdfReader(open(file_location, 'rb'))
 for page_num in range(len(pdf_reader.pages)):
     text = pdf_reader.pages[page_num].extract_text()
     text_cleaned = text.replace('\n', '')
+    text_list.append(text_cleaned)
 
 file_name = file_location.split('/')[-1].split('.')[0]
 
+print('Recording ...')
 time.sleep(15)
 
-recorder = gTTS(text=text_cleaned, lang=language).save(f'{file_name}.mp3')
+recorder = gTTS(text=",".join(text_list), lang=language).save(f'{file_name}.mp3')
+print('Done!')
